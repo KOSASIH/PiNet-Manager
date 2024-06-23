c525b37ed0087d4140b09d1fe269ad308c517d75
KOSASIH <kosasihg88@gmail.com>
2024-06-23 10:17:05 +0700

    Create user-interface.js

---
+++ user-interface/user-interface.js
@@ -0,0 +1,17 @@
+// user-interface.js
+class UIManager {
+  constructor() {
+    this.mobileApp = true;
+    this.webInterface = true;
+  }
+
+  createMobileApp() {
+    // mobile app creation logic here
+  }
+
+  createWebInterface() {
+    // web interface creation logic here
+  }
+}
+
+export default UIManager;
