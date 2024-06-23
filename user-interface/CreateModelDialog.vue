016ed30e9d6e3fc1517630ccdd8892f69c99a8ea
KOSASIH <kosasihg88@gmail.com>
2024-06-23 11:43:02 +0700

    Create CreateModelDialog.vue

---
+++ user-interface/CreateModelDialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Create Model Dialog</h2>
+    <form @submit.prevent="createModel">
+      <label>IP Address:</label>
+      <input type="text" v-model="ipAddress" />
+      <br />
+      <label>Host Name:</label>
+      <input type="text" v-model="hostName" />
+      <br />
+      <button type="submit">Create Model</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ipAddress: "",
+      hostName: ""
+    };
+  },
+  methods: {
+    createModel() {
+      // Call API to create model
+      console.log("Model created successfully!");
+    }
+  }
+};
+</script>
