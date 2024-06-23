bf6feb350ca3b330f0f0fa9a3a106f5722325122
KOSASIH <kosasihg88@gmail.com>
2024-06-23 10:16:14 +0700

    Create scalability.js

---
+++ scalability/scalability.js
@@ -0,0 +1,17 @@
+// scalability.js
+class ScalabilityManager {
+  constructor() {
+    this.horizontalScaling = true;
+    this.verticalScaling = true;
+  }
+
+  scaleHorizontally() {
+    // horizontal scaling logic here
+  }
+
+  scaleVertically() {
+    // vertical scaling logic here
+  }
+}
+
+export default ScalabilityManager;
