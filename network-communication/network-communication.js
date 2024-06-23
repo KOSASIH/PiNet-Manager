be2ff98408d9b62aa673f4ffbe5e742c6b87c5f1
KOSASIH <kosasihg88@gmail.com>
2024-06-23 10:14:07 +0700

    Create network-communication.js

---
+++ network-communication/network-communication.js
@@ -0,0 +1,16 @@
+// network-communication.js
+class NetworkCommunicator {
+  constructor() {
+    this.nodes = [];
+  }
+
+  nodeToNodeCommunication() {
+    // node-to-node communication logic here
+  }
+
+  nodeToClientCommunication() {
+    // node-to-client communication logic here
+  }
+}
+
+export default NetworkCommunicator;
