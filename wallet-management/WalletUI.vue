9f0342a0ae5653574743d7bf7772a6de8010075a
KOSASIH <kosasihg88@gmail.com>
2024-06-23 11:51:01 +0700

    Create WalletUI.vue

---
+++ wallet-management/WalletUI.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Wallet</h2>
+    <form @submit.prevent="createWallet">
+      <label>Wallet Name:</label>
+      <input type="text" v-model="walletName" />
+      <br />
+      <button type="submit">Create Wallet</button>
+    </form>
+    <ul>
+      <li v-for="wallet in wallets" :key="wallet.address">
+        {{ wallet.address }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      walletName: "",
+      wallets: []
+    };
+  },
+  methods: {
+    createWallet() {
+      // Call API to create wallet
+      console.log("Wallet created successfully!");
+    }
+  }
+};
+</script>
